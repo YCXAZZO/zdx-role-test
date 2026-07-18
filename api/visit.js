8f9542d380dc5272b9664f1d4f2606605be604ed
YCXAZZO <69510673+YCXAZZO@users.noreply.github.com>
2026-07-18 16:44:25 +0800

    Update visit.js

--- api/visit.js
+++ api/visit.js
@@ -3,6 +3,7 @@ import { redis } from '../lib/redis.js';
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     try {
+        await redis.set('visit_count', 0);
         const count = await redis.incr('visit_count');
         res.status(200).json({ count });
     } catch (error) {
